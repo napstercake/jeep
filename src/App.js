f8c4a39ff7013235a430c37f6ab4be74870ccd7e
Ricardo Gonzales <ricardogonzales@Ricardos-MacBook-Pro.local>
2017-06-29 13:08:11 -0500

    First layout css

--- src/App.js
+++ src/App.js
@@ -1,57 +1,95 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+// CSS
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="wrapper">
-        <div className="wrapper-header">
-          <header>
-            <nav>
-              <ul>
-                <li><a>About me</a></li>
-                <li><a>Contact</a></li>
-              </ul>
-            </nav>
-          </header>
-          <div className="wrapper-main">
-            <main>
-              <div className="container">
-                <div className="counter">
-                  $ 1.45
+        <nav className="nav">
+          <button className="donate">Donate</button>
+          <ul>
+            <li><a href="">About me</a></li>
+          </ul>
+          <div className="clear"></div>
+        </nav>
+        <div className="wrapper-main">
+          <main>
+            <div className="container">
+              <div className="counter">
+                $ 1.45
+              </div>
+              <div className="goal">
+                $ 27,000.00
+              </div>
+              <div className="comments">
+                <div className="comment">
+                  Hope you get it!
                 </div>
-                <div className="goal">
-                  $ 27,000.00
+                <div className="comment">
+                  Stupid idea, still works! :)
                 </div>
-                <div className="comments">
-                  <div className="comment">
-                    this is the comment 1
-                  </div>
-                  <div className="comment">
-                    this is the comment 2
-                  </div>
-                  <div className="comment">
-                    this is the comment 3
-                  </div>
-                  <div className="comment">
-                    this is the comment 4
-                  </div>
-                  <div className="comment">
-                    this is the comment 5
-                  </div>
+                <div className="comment">
+                  Give me a ride
+                </div>
+                <div className="comment">
+                  Hope yo will get it
+                </div>
+                <div className="comment">
+                  rocks!
+                </div>
+                <div className="comment">
+                  Hope you get it!
+                </div>
+                <div className="comment">
+                  Stupid idea, still works! :)
+                </div>
+                <div className="comment">
+                  Give me a ride
+                </div>
+                <div className="comment">
+                  Hope yo will get it
+                </div>
+                <div className="comment">
+                  rocks!
+                </div>
+                <div className="comment">
+                  Hope you get it!
+                </div>
+                <div className="comment">
+                  Stupid idea, still works! :)
+                </div>
+                <div className="comment">
+                  Give me a ride
+                </div>
+                <div className="comment">
+                  Hope yo will get it
+                </div>
+                <div className="comment">
+                  rocks!
+                </div>
+                <div className="comment">
+                  Hope you get it!
+                </div>
+                <div className="comment">
+                  Stupid idea, still works! :)
+                </div>
+                <div className="comment">
+                  Give me a ride
+                </div>
+                <div className="comment">
+                  Hope yo will get it
+                </div>
+                <div className="comment">
+                  rocks!
                 </div>
               </div>
-            </main>
-            <footer>
-              <ul>
-                <li><a>Link 1</a> </li>
-                <li><a>Link 2</a> </li>
-              </ul>
-            </footer>
+            </div>
+          </main>
           </div>
-        </div>
-      </div>    
+      </div>  
+      
     );
   }
 }

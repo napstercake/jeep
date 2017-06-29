7182633be14a06327770219793f90b338deb353e
Ricardo Gonzales <ricardogonzales@Ricardos-MacBook-Pro.local>
2017-06-28 23:58:30 -0500

    First layouts

--- src/App.js
+++ src/App.js
@@ -5,15 +5,53 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="wrapper">
+        <div className="wrapper-header">
+          <header>
+            <nav>
+              <ul>
+                <li><a>About me</a></li>
+                <li><a>Contact</a></li>
+              </ul>
+            </nav>
+          </header>
+          <div className="wrapper-main">
+            <main>
+              <div className="container">
+                <div className="counter">
+                  $ 1.45
+                </div>
+                <div className="goal">
+                  $ 27,000.00
+                </div>
+                <div className="comments">
+                  <div className="comment">
+                    this is the comment 1
+                  </div>
+                  <div className="comment">
+                    this is the comment 2
+                  </div>
+                  <div className="comment">
+                    this is the comment 3
+                  </div>
+                  <div className="comment">
+                    this is the comment 4
+                  </div>
+                  <div className="comment">
+                    this is the comment 5
+                  </div>
+                </div>
+              </div>
+            </main>
+            <footer>
+              <ul>
+                <li><a>Link 1</a> </li>
+                <li><a>Link 2</a> </li>
+              </ul>
+            </footer>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </div>    
     );
   }
 }
